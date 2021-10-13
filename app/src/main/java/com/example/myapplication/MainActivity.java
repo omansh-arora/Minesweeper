2283a443d9528b0aa9efd993784182ad240b2f89
gsnagra <gavin_nagra@sfu.ca>
2021-10-13 04:37:55 +0000

    Update .idea/misc.xml, app/src/main/java/com/example/myapplication/MainActivity.java files

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< app/src/main/java/com/example/myapplication/MainActivity.java
         h.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -26,10 +25,5 @@ public class MainActivity extends AppCompatActivity {
 
                     setTitle("Main Menu");
 
-
-=======
-
-        setTitle("Main Menu");
->>>>>>> app/src/main/java/com/example/myapplication/MainActivity.java
     }
 }
