c582566120642dcc99108716f5ccc74d0878e445
Omansh <oarora@sfu.ca>
2021-10-13 18:07:49 -0700

    Help screen added

---
+++ app/src/main/java/com/example/myapplication/HelpActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        setTitle("Help");
+        TextView t2 = (TextView) findViewById(R.id.help_txtHomeLink);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
