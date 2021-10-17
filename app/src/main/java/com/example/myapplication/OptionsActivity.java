03f580cf04223e6c00cccc011bc7b5b3ff580ea8
enigma-3 <gavin.98@hotmail.com>
2021-10-16 23:14:20 -0700

    Fixed layout for Options Board Size

--- app/src/main/java/com/example/myapplication/OptionsActivity.java
+++ app/src/main/java/com/example/myapplication/OptionsActivity.java
@@ -22,21 +22,21 @@ public class OptionsActivity extends AppCompatActivity {
         RadioGroup board_group = (RadioGroup) findViewById(R.id.radio_group_board_size);
         RadioGroup mines_group = (RadioGroup) findViewById(R.id.radio_group_mines);
 
-        int[] board_Size = getResources().getIntArray(R.array.num_board_size);
+        String[] board_Size = getResources().getStringArray(R.array.num_board_size);
         int[] number_of_mines = getResources().getIntArray(R.array.num_mines_size);
 
         // Create the buttons
         for(int i = 0; i < board_Size.length; i++) {
-            final int boardSize = board_Size[i];
+            final String boardSize = board_Size[i];
 
             RadioButton board_button = new RadioButton(this);
-            board_button.setText(getString(R.string.board_size, boardSize));
+            board_button.setText(boardSize);
 
             // Set on-click callbacks
             board_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(OptionsActivity.this, "You clicked " + boardSize,
+                    Toast.makeText(OptionsActivity.this, "Size " + boardSize,
                             Toast.LENGTH_SHORT).show();
                 }
             });
