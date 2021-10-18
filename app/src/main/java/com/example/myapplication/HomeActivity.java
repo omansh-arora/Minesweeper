bf20d246a49b46e0972f1f1d0a2ac19e7a5efe16
enigma-3 <gavin.98@hotmail.com>
2021-10-18 00:10:20 -0700

    Added in skip button to welcome screen

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -5,14 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
     Handler h = new Handler();
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        button = (Button) findViewById(R.id.skip);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
 
         h.postDelayed(new Runnable() {
             @Override
@@ -23,4 +34,10 @@ public class HomeActivity extends AppCompatActivity {
             }
         }, 3000);
     }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
