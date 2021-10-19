895ab1bd8d92a89ea0037378c9fdeb4e3aaad92c
enigma-3 <gavin.98@hotmail.com>
2021-10-19 00:11:15 -0700

    Added in fade in/out animations for welcome screen

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -12,15 +12,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
     Handler h = new Handler();
+    private static int time = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(in);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, time);
 
         Button button = (Button) findViewById(R.id.skip);
         button.setOnClickListener(new View.OnClickListener() {
